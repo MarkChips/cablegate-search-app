ccee5104c5f3a5c4b6d970cba0d9a808089f4f2e
Mark Chipperfield <mark-chipperfield@outlook.com>
2025-03-19 21:47:12 +0000

    feat: add function to read html files

--- parse.js
+++ parse.js
@@ -1,1 +1,21 @@
+const fs = require('fs').promises;
+const path = require('path');
 
+async function getHtmlFiles(dir) {
+    let files = [];
+    const items = await fs.readdir(dir, { withFileTypes: true });
+    for (const item of items) {
+        const fullPath = path.join(dir, item.name);
+        if (item.isDirectory()) {
+            files = files.concat(await getHtmlFiles(fullPath));
+        } else if (item.name.endsWith('.html')) {
+            files.push(fullPath);
+        }
+    }
+    return files;
+}
+
+(async () => {
+    const htmlFiles = await getHtmlFiles('cable');
+    console.log(`Found ${htmlFiles.length} HTML files`);
+})();
