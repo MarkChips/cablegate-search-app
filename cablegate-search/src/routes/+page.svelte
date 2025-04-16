90cf854e6cc4ce58e55a7f34389ce2be7af29047
Mark Chipperfield <mark-chipperfield@outlook.com>
2025-04-16 18:28:44 +0100

    style: improve form look and layout
    add dropdown options for classification, alter h1, improve grid layout

--- cablegate-search/src/routes/+page.svelte
+++ cablegate-search/src/routes/+page.svelte
@@ -62,60 +62,152 @@
 	}
 </script>
 
-<h1 class="text-3xl font-bold mb-4">Cablegate Search</h1>
+<h1
+	class="my-2 text-center pb-2 text-2xl/7 font-bold text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight"
+>
+	Cablegate Search
+</h1>
 
-{#if error}
-	<p class="text-red-600">{error}</p>
-{/if}
+<form on:submit|preventDefault={search} class="grid gap-4 max-w-xl mx-5 sm:mx-auto">
+	<div class="grid grid-cols-1 gap-x-6 gap-y-2 sm:grid-cols-6">
+		<div class="col-span-full">
+			<label for="keywords" class="block text-sm/6 font-medium text-gray-900">Search: </label>
+			<div class="mt-2">
+				<input
+					type="text"
+					bind:value={$searchParams.q}
+					name="keywords"
+					id="keywords"
+					class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+				/>
+			</div>
+		</div>
 
-{#if loading}
-	<p>Loading...</p>
-{/if}
+		<div class="sm:col-span-3">
+			<label for="year" class="block text-sm/6 font-medium text-gray-900">Year: </label>
+			<div class="mt-2">
+				<input
+					type="number"
+					min="1966"
+					max="2006"
+					bind:value={$searchParams.year}
+					name="year"
+					id="year"
+					class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+				/>
+			</div>
+		</div>
 
-<form on:submit|preventDefault={search} class="grid gap-4 max-w-xl mx-auto">
-	<label class="flex flex-col gap-1">
-		Search:
-		<input type="text" bind:value={$searchParams.q} class="p-2 border rounded" />
-	</label>
-	<label class="flex flex-col gap-1">
-		Year:
-		<input
-			type="number"
-			min="1900"
-			max="2100"
-			bind:value={$searchParams.year}
-			class="p-2 border rounded"
-		/>
-	</label>
-	<label class="flex flex-col gap-1">
-		Month:
-		<input
-			type="number"
-			min="1"
-			max="12"
-			bind:value={$searchParams.month}
-			class="p-2 border rounded"
-		/>
-	</label>
-	<label class="flex flex-col gap-1">
-		From:
-		<input type="text" bind:value={$searchParams.from} class="p-2 border rounded" />
-	</label>
-	<label class="flex flex-col gap-1">
-		To:
-		<input type="text" bind:value={$searchParams.to} class="p-2 border rounded" />
-	</label>
-	<label class="flex flex-col gap-1">
-		Tags (comma-separated):
-		<input type="text" bind:value={$searchParams.tags} class="p-2 border rounded" />
-	</label>
-	<label class="flex flex-col gap-1">
-		Classification:
-		<input type="text" bind:value={$searchParams.classification} class="p-2 border rounded" />
-	</label>
-	<button type="submit" disabled={loading} class="p-2 bg-blue-600 text-white rounded">
-		Search
-	</button>
+		<div class="sm:col-span-3">
+			<label for="month" class="block text-sm/6 font-medium text-gray-900">Month: </label>
+			<div class="mt-2">
+				<input
+					type="number"
+					min="1"
+					max="12"
+					bind:value={$searchParams.month}
+					name="month"
+					id="month"
+					class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+				/>
+			</div>
+		</div>
+
+		<div class="col-span-full">
+			<label for="from" class="block text-sm/6 font-medium text-gray-900">From: </label>
+			<div class="mt-2">
+				<input
+					type="text"
+					bind:value={$searchParams.from}
+					name="from"
+					id="from"
+					class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+				/>
+			</div>
+		</div>
+
+		<div class="col-span-full">
+			<label for="to" class="block text-sm/6 font-medium text-gray-900">To: </label>
+			<div class="mt-2">
+				<input
+					type="text"
+					bind:value={$searchParams.to}
+					name="to"
+					id="to"
+					class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+				/>
+			</div>
+		</div>
+
+		<div class="col-span-full">
+			<label for="tags" class="block text-sm/6 font-medium text-gray-900"
+				>Tags (comma-separated):
+			</label>
+			<div class="mt-2">
+				<input
+					type="text"
+					bind:value={$searchParams.tags}
+					name="tags"
+					id="tags"
+					class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+				/>
+			</div>
+		</div>
+
+		<div class="flex col-span-full items-end">
+			<div class="sm:w-1/3">
+				<label for="classification" class="block text-sm/6 font-medium text-gray-900"
+					>Classification:
+				</label>
+				<div class="mt-2 grid">
+					<select
+						bind:value={$searchParams.classification}
+						name="classification"
+						id="classification"
+						class="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+					>
+						<option></option>
+						<option>UNCLASSIFIED</option>
+						<option>CONFIDENTIAL</option>
+						<option>SECRET</option>
+					</select>
+					<svg
+						class="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
+						viewBox="0 0 16 16"
+						fill="currentColor"
+						aria-hidden="true"
+						data-slot="icon"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+				</div>
+			</div>
+
+			<div class="sm:mx-6 mx-2">
+				<button
+					type="submit"
+					disabled={loading}
+					class="rounded-md bg-indigo-500 hover:bg-indigo-700 px-5 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+				>
+					Search
+				</button>
+			</div>
+
+			<div>
+				{#if error}
+					<p class="text-red-600">{error}</p>
+				{/if}
+
+				{#if loading}
+					<p>Loading...</p>
+				{/if}
+			</div>
+		</div>
+	</div>
 </form>
 
 {#if results.length}
