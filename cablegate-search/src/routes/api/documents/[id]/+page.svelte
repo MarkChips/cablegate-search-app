0a6f0d258fdbcf8e3a4a753fde6ae4b9b0c7c08b
Mark Chipperfield <mark-chipperfield@outlook.com>
2025-04-17 16:45:03 +0100

    style: improve margins of document pages
    remove body h3 title, improve look of 'back to search' button

--- cablegate-search/src/routes/api/documents/[id]/+page.svelte
+++ cablegate-search/src/routes/api/documents/[id]/+page.svelte
@@ -24,7 +24,7 @@
 	});
 </script>
 
-<h1 class="text-3xl font-bold mb-4">Document Details</h1>
+<h1 class="text-3xl font-bold mx-4">Document Details</h1>
 
 {#if loading}
 	<p>Loading...</p>
@@ -35,16 +35,23 @@
 {/if}
 
 {#if document}
-	<h2 class="text-xl">{document.subject || 'No Subject'}</h2>
-	<p><strong>ID:</strong> {document._id}</p>
-	<p><strong>Created:</strong> {new Date(document.created).toLocaleDateString()}</p>
-	<p><strong>Released:</strong> {new Date(document.released).toLocaleDateString()}</p>
-	<p><strong>Classification:</strong> {document.classification}</p>
-	<p><strong>Origin:</strong> {document.origin}</p>
-	<p><strong>To:</strong> {document.to || 'N/A'}</p>
-	<p><strong>Tags:</strong> {document.tags?.join(', ') || 'None'}</p>
-	<p><strong>Keywords:</strong> {document.keywords?.join(', ') || 'None'}</p>
-	<h3 class="text-lg mt-4">Body</h3>
+	<h2 class="mx-4 text-xl">{document.subject || 'No Subject'}</h2>
+	<div class="m-4">
+		<p><strong>ID:</strong> {document._id}</p>
+		<p><strong>Created:</strong> {new Date(document.created).toLocaleDateString()}</p>
+		<p><strong>Released:</strong> {new Date(document.released).toLocaleDateString()}</p>
+		<p><strong>Classification:</strong> {document.classification}</p>
+		<p><strong>Origin:</strong> {document.origin}</p>
+		<p><strong>To:</strong> {document.to || 'N/A'}</p>
+		<p><strong>Tags:</strong> {document.tags?.join(', ') || 'None'}</p>
+		<p><strong>Keywords:</strong> {document.keywords?.join(', ') || 'None'}</p>
+	</div>
 	<pre class="whitespace-pre-wrap bg-gray-100 p-4 rounded">{document.body}</pre>
-	<a href="/" class="text-blue-600 hover:underline">Back to Search</a>
+	<div class="m-6">
+		<a
+			href="/"
+			class="rounded-md bg-indigo-500 hover:bg-indigo-700 px-5 py-2 text-sm font-semibold text-white shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+			>Back to Search</a
+		>
+	</div>
 {/if}
